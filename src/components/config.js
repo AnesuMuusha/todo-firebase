8608b8e6a4ab599dc452b064f5a375d3cff2bf6a
AnesuMuusha <anesumuusha@gmail.com>
2024-06-15 20:57:15 +0200

    added Firebase auth to my the component

--- src/components/config.js
+++ src/components/config.js
@@ -1,17 +1,21 @@
 import { initializeApp } from "firebase/app";
-import {getAuth,GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase, ref, onValue, set, push, remove, update } from "firebase/database";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCYy2QwMKllMeywy3cGS0nBuvIs8kKHq54",
-    authDomain: "todo-firebase-4ffc5.firebaseapp.com",
-    projectId: "todo-firebase-4ffc5",
-    storageBucket: "todo-firebase-4ffc5.appspot.com",
-    messagingSenderId: "477744282064",
-    appId: "1:477744282064:web:48e5cdf99824083a9d21d5",
-    measurementId: "G-84988ZJEX3"
+  apiKey: "AIzaSyCYy2QwMKllMeywy3cGS0nBuvIs8kKHq54",
+  authDomain: "todo-firebase-4ffc5.firebaseapp.com",
+  projectId: "todo-firebase-4ffc5",
+  storageBucket: "todo-firebase-4ffc5.appspot.com",
+  messagingSenderId: "477744282064",
+  appId: "1:477744282064:web:48e5cdf99824083a9d21d5",
+  measurementId: "G-84988ZJEX3",
+  databaseURL: "https://todo-firebase-4ffc5-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-export {auth,provider};
+const db = getDatabase(app);
+
+export { auth, provider, db };
